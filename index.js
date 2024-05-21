232fe91762b4b1709adcfe8c4b474d5095a0ab3a
Vitor <vitorkamenschek@gmail.com>
2024-05-21 15:26:23 -0300

    First commit

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-/*let name = "Vitor";
-let age = 18;
-let online = true;
-
-document.getElementById("p1").textContent = `My name is ${name}`;
-document.getElementById("p2").textContent = `I am ${age} years old`;
-document.getElementById("p3").textContent = `I am ${online ? "online" : "offline"}`;
-*/
-
 let username;
 
-username = window.prompt(`What is your name?`);
-
-document.getElementById("p1").textContent = `My name is ${username}`;
+document.getElementById("submit").onclick = function() {
+    username = document.getElementById("username").value;
+    document.getElementById("myTitle").textContent = `Hello ${username}`;
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+}
